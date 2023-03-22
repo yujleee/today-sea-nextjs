4a597a9974bbf05741e676028eb17544cc05a0a7
LeeYujeong <yujlee242@gmail.com>
2023-03-22 23:15:40 +0900

    ✨ setting meta tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,8 @@
-import '../styles/globals.css';
-import '../styles/main.css';
 import type { AppProps } from 'next/app';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { RecoilRoot } from 'recoil';
+import Head from 'next/head';
+import '../styles/main.scss';
 
 const client = new QueryClient({
   defaultOptions: {
@@ -16,6 +16,9 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
       <QueryClientProvider client={client}>
+        <Head>
+          <title>오늘의 바다</title>
+        </Head>
         <Component {...pageProps} />
       </QueryClientProvider>
     </RecoilRoot>
