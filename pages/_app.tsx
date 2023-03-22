e94bdd0e11c0188f1cd677de5f0b8d1e641fa93c
LeeYujeong <yujlee242@gmail.com>
2023-03-22 11:57:06 +0900

    📦 add recoil package and setting react-query, recoil

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,25 @@
 import '../styles/globals.css';
 import '../styles/main.css';
 import type { AppProps } from 'next/app';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { RecoilRoot } from 'recoil';
+
+const client = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <RecoilRoot>
+      <QueryClientProvider client={client}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
+    </RecoilRoot>
+  );
 }
 
 export default MyApp;
