679bc23d5871ece79c7add1e616d6591b24ea351
LeeYujeong <yujlee242@gmail.com>
2023-03-25 22:01:59 +0900

    💄 Add search UI

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,20 @@
 import type { NextPage } from 'next';
 import Clouds from '../components/common/Clouds';
 import Waves from '../components/common/Waves';
+import Search from '../components/home/Search';
 
 const Home: NextPage = () => {
   return (
     <div>
-      <main>
-        <Clouds />
-        <Waves />
+      <Clouds />
+      <main className="main">
+        <div className="title-wrapper">
+          <h1 className="title">오늘의 바다</h1>
+          <p className="desc-text">해수욕장을 검색해 일기 예보를 확인해보세요.</p>
+        </div>
+        <Search />
       </main>
+      <Waves />
     </div>
   );
 };
