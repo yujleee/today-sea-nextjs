80da12a4a940a8d212cd310187c6a9318ffd25d6
LeeYujeong <yujlee242@gmail.com>
2023-04-19 21:45:03 +0900

    feat: 데이터 타입 추가

--- global.d.ts
+++ global.d.ts
@@ -15,3 +15,31 @@ interface Now {
   date: string;
   time: string;
 }
+
+interface Forecast {
+  baseDate: string;
+  baseTime: string;
+  beachNum: string;
+  category: string;
+  fcstDate: string;
+  fcstTime: string;
+  fcstValue: string;
+  nx: number;
+  ny: number;
+}
+
+interface Sun {
+  beachNum: string;
+  baseDate: string;
+  sunrise: string;
+  sunset: string;
+}
+
+interface Tide {
+  beachNum: string;
+  baseDate: string;
+  tiStnld: string;
+  tiTime: string;
+  tiType: string;
+  tilevel: string;
+}
